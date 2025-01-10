80a8ba9970d457ffb5df864140e03997d1f5d67f
rizkyark <rizkyark99@gmail.com>
2025-01-10 22:52:33 +0700

    Add: create mockup Sign In page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,68 @@
-import React from "react";
-import { Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
+import { icons } from "@/constant";
+import { Link } from "expo-router";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignIn = () => {
+  const banner = "https://dummyimage.com/750x464/000/fff.png";
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>Sign In</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        {/* Banner */}
+        <View className="relative w-full h-[250px]">
+          <Image source={{ uri: banner }} className="z-0 w-full h-[250px]" />
+          <Text className="text-2xl text-white font-JakartaSemiBold absolute bottom-3 left-3">
+            Welcome👋
+          </Text>
+        </View>
+
+        {/* Input Form */}
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            secureTextEntry={true}
+            onChangeText={(value) => setForm({ ...form, password: value })}
+          />
+
+          <CustomButton
+            title="Sign In"
+            className="mt-8"
+            onPress={onSignInPress}
+          />
+
+          <OAuth />
+
+          <Link
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Don't have an account? </Text>
+            <Text className="text-primary-500"> Sign Up</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+import { icons } from "@/constant";
 
-const OAuth = () => (
-  <View>
-    <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
-      <View className="flex-1 h-[1px] bg-general-100"></View>
-      <Text className="text-lg">Or</Text>
-      <View className="flex-1 h-[1px] bg-general-100"></View>
+const OAuth = () => {
+  const handleGoogleSignIn = async () => {};
+
+  return (
+    <View>
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+        <View className="flex-1 h-[1px] bg-general-100"></View>
+        <Text className="text-lg">Or</Text>
+        <View className="flex-1 h-[1px] bg-general-100"></View>
+      </View>
+
+      <CustomButton
+        title="Log In with Google"
+        className="mt-5 w-full shadow-none"
+        bgVariant="outline"
+        textVariant="primary"
+        IconLeft={() => (
+          <Image
+            source={icons.google}
+            resizeMode="contain"
+            className="w-5 h-5 mx-2"
+          />
+        )}
+        onPress={handleGoogleSignIn}
+      />
     </View>
-  </View>
-);
+  );
+};
 
 export default OAuth;

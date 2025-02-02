47dc315cc748872168255668d622bb6cf4bcc019
rizkyark <rizkyark99@gmail.com>
2025-02-03 00:29:57 +0700

    Add: Sign up verification email

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,21 +1,92 @@
 import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
-import { icons } from "@/constant";
-import { Link } from "expo-router";
+import { icons, images } from "@/constant";
+import { useSignUp } from "@clerk/clerk-expo";
+import { Link, router } from "expo-router";
 import React, { useState } from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Alert, Image, ScrollView, Text, View } from "react-native";
+import ReactNativeModal from "react-native-modal";
+import Modal from "react-native-modal";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUp = () => {
+  const { isLoaded, signUp, setActive } = useSignUp();
   const banner = "https://dummyimage.com/750x464/000/fff.png";
   const [form, setForm] = useState({
     name: "",
     email: "",
     password: "",
   });
+  const [verification, setVerification] = useState({
+    state: "default",
+    error: "",
+    code: "",
+    testing: false,
+  });
 
-  const onSignUpPress = async () => {};
+  const testButton = () => {
+    return setVerification({ ...verification, testing: true });
+  };
+
+  const onSignUpPress = async () => {
+    if (!isLoaded) return;
+
+    // Start sign-up process using email and password provided
+    try {
+      await signUp.create({
+        emailAddress: form.email,
+        password: form.password,
+      });
+
+      // Send user an email with verification code
+      await signUp.prepareEmailAddressVerification({ strategy: "email_code" });
+
+      // Set 'pendingVerification' to true to display second form
+      // and capture OTP code
+      setVerification({ ...verification, state: "pending" });
+    } catch (err: any) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      // console.error(JSON.stringify(err, null, 2));
+      Alert.alert("Error", err.errors[0].longMessage);
+    }
+  };
+
+  // Handle submission of verification form
+  const onVerifyPress = async () => {
+    if (!isLoaded) return;
+
+    try {
+      // Use the code the user provided to attempt verification
+      const signUpAttempt = await signUp.attemptEmailAddressVerification({
+        code: verification.code,
+      });
+
+      // If verification was completed, set the session to active
+      // and redirect the user
+      if (signUpAttempt.status === "complete") {
+        await setActive({ session: signUpAttempt.createdSessionId });
+        setVerification({ ...verification, state: "success" });
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        setVerification({
+          ...verification,
+          error: "Verification failed",
+          state: "failed",
+        });
+      }
+    } catch (err: any) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      setVerification({
+        ...verification,
+        error: err.errors[0].longMessage,
+        state: "failed",
+      });
+    }
+  };
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -24,7 +95,7 @@ const SignUp = () => {
         <View className="relative w-full h-[250px]">
           <Image source={{ uri: banner }} className="z-0 w-full h-[250px]" />
           <Text className="text-2xl text-white font-JakartaSemiBold absolute bottom-3 left-3">
-            Create Your Account
+            Create Your Account {form.password}
           </Text>
         </View>
 
@@ -68,6 +139,69 @@ const SignUp = () => {
             <Text>Already have an account? </Text>
             <Text className="text-primary-500"> Log In</Text>
           </Link>
+
+          <ReactNativeModal
+            isVisible={verification.state === "pending"}
+            onModalHide={() =>
+              setVerification({ ...verification, state: "success" })
+            }
+          >
+            <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+              <Text className="text-2xl font-JakartaExtraBold mb-2">
+                Verification
+              </Text>
+              <Text className="font-Jakarta mb-5">
+                We've sent a verification code to {form.email}
+              </Text>
+              <InputField
+                label="Code"
+                icon={icons.lock}
+                placeholder="12345"
+                value={verification.code}
+                keyboardType="numeric"
+                onChangeText={(code) =>
+                  setVerification({ ...verification, code })
+                }
+              />
+
+              {verification.error && (
+                <Text className="text-red-500 text-sm mt-1">
+                  {verification.error}
+                </Text>
+              )}
+
+              <CustomButton
+                title="Verify Email"
+                onPress={onVerifyPress}
+                className="mt-5 bg-success-500"
+              />
+            </View>
+          </ReactNativeModal>
+
+          <ReactNativeModal
+            isVisible={verification.state === "success"}
+            onBackdropPress={() =>
+              setVerification({ ...verification, state: "default" })
+            }
+          >
+            <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+              <Image
+                source={images.check}
+                className="w-[110px] h-[110px] mx-auto my-5"
+              />
+              <Text className="text-3xl font-JakartaBold text-center">
+                Verified
+              </Text>
+              <Text className="text-base text-gray-400 font-Jakarta text-center">
+                You have successfully verified your account
+              </Text>
+              <CustomButton
+                title="Browse Home"
+                className="mt-5"
+                onPress={() => router.replace("/(root)/(tabs)/home")}
+              />
+            </View>
+          </ReactNativeModal>
         </View>
       </View>
     </ScrollView>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
+import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
+import { tokenCache } from "@/cache";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -19,6 +21,14 @@ export default function RootLayout() {
     "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
   });
 
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+  }
+
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
@@ -31,11 +41,15 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(root)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(root)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
